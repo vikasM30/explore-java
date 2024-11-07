d7026c6491510ff4c07c97e0bba9ce97b133244a
vikasM30 <mishraviki.mishra@gmail.com>
2024-11-07 13:26:16 +0530

    collection basics 2

--- CoreJava/Collections/SetCollection.java
+++ CoreJava/Collections/SetCollection.java
@@ -1,12 +1,18 @@
 package CoreJava.Collections;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class SetCollection {
     public static void main(String[] args) {
 
+
+        System.out.println("Un-sorted: Hash Set");
+        
         // un-sorted
         Set<Integer> l1 = new HashSet<Integer>();
         l1.add(4);
@@ -19,7 +25,7 @@ public class SetCollection {
             System.out.println(val);
         }
 
-        System.out.println();
+        System.out.println("Sorted: Tree Set");
 
         // Sorted
         Set<Integer> l2 = new TreeSet<Integer>();
@@ -28,6 +34,33 @@ public class SetCollection {
         l2.add(22);
         l2.add(14);
         l2.add(4);
+
+        for (Integer val : l2) {
+            System.out.println(val);
+        }
+
+        System.out.println("Use of Iterator: Tree Set");
+
+        // use of iterator
+        Collection<Integer> l3 = new TreeSet<Integer>();
+        l3.add(4);
+        l3.add(43);
+        l3.add(22);
+
+        Iterator<Integer> values = l3.iterator(); 
+
+        while (values.hasNext()) {
+            System.out.println(values.next());
+        }
+
+        System.out.println("Sorted: Linked Hash Set");
+
+        Collection<Integer> l4 = new LinkedHashSet<Integer>();
+        l4.add(4);
+        l4.add(43);
+        l4.add(22);
+        l4.add(14);
+        l4.add(4);
 
         for (Integer val : l2) {
             System.out.println(val);
