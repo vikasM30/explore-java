0d760eb8d96bab48b5517445ccd96b9e58cb74b0
vikasM30 <mishraviki.mishra@gmail.com>
2024-11-08 15:41:28 +0530

    a simplified way of printing sout without psvm

---
+++ SimplifiedMain.java
@@ -0,0 +1,10 @@
+class SimplifiedMain {
+
+    // to run this code, since its preview feature of java 21, hence: execute commands:
+    // 1.) javac --enable-preview --release 21 SimplifiedMain.java  
+    // 2.) java --enable-preview SimplifiedMain
+    void main() {
+       System.out.println("Hello, world!");
+   }
+
+}
